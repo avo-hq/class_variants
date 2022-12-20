f2e59ee5f015c52e8b5506dcb825d7f863ef86a6
Will Cosgrove <will@cosgrove.email>
2022-12-20 09:41:58 -0600

    Add support for boolean variants
    Boolean variants work by defining a variant and setting it's value to a
    String.
    
    ```ruby
    ClassVariants.build(
      variants: {
        block: "w-full justify-center"
      },
      defaults: {
        block: false
      }
    )
    ```
    
    This will only render the classes if you call render with `block: true`.
    
    You can also define the inverse by prepending the variant key with "!".
    
    ```ruby
    classes = ClassVariants.build(
      variants: {
        block: "w-full justify-center"
        "!block": "w-auto"
      },
      defaults: {
        block: false
      }
    )
    
    classes.render # => "w-auto"
    classes.render(block: true) # => "w-full justify-center"
    ```

--- lib/class_variants/instance.rb
+++ lib/class_variants/instance.rb
@@ -5,7 +5,7 @@ class ClassVariants::Instance
 
   def initialize(classes = "", variants: {}, defaults: {})
     @classes = classes
-    @variants = variants
+    @variants = expand_boolean_variants(variants)
     @defaults = defaults
   end
 
@@ -25,5 +25,21 @@ class ClassVariants::Instance
 
     # Return the final token list
     result.join " "
+  end
+
+  private
+
+  def expand_boolean_variants(variants)
+    variants.each.map { |key, value|
+      case value
+      when String
+        s_key = key.to_s
+        { s_key.delete_prefix("!").to_sym => { !s_key.start_with?("!") => value } }
+      else
+        { key => value }
+      end
+    }.reduce do |variants, more_variants|
+      variants.merge!(more_variants) { |_key, v1, v2| v1.merge!(v2) }
+    end
   end
 end
