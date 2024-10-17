c6129c127f4fb18f5c27fd30bf2dc8d49b34181c
Javier Aranda <javier@aranda.dev>
2024-10-17 18:53:31 +0200

    Add helper module for defining variants

--- lib/class_variants.rb
+++ lib/class_variants.rb
@@ -1,6 +1,7 @@
 require "class_variants/version"
 require "class_variants/action_view/helpers"
 require "class_variants/instance"
+require "class_variants/helper"
 require "class_variants/railtie" if defined?(Rails)
 
 module ClassVariants
